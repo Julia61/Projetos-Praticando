8f8c9193c170bd99457cf982e8db07ee1990a566
Julia <juliapinheiro620@gmail.com>
2024-11-21 10:49:25 -0300

    novas configurações

--- projeto01/scripts/script.js
+++ projeto01/scripts/script.js
@@ -2,8 +2,22 @@ function envio(){
     var filmeEscolha = document.getElementById('filme').value
     var catalago = document.querySelector('p#pergunta')
     if(filmeEscolha.length == 0){
-        alert('errado')
+        alert('[ERRO].Tente novamente!')
     }else{
-        catalago.innerHTML = `seu filme ${filmeEscolha}`
+        catalago.innerHTML = `Legal!! você gosta de (${filmeEscolha}). Gostaria de ver o nosso catalago?`
+        
+        var botaoSim = document.createElement('button')
+        var botaoNao = document.createElement('button')
+        botaoSim.textContent = 'Sim'
+        botaoNao.textContent = "Não"
+        botaoSim.onclick = function(){
+            catalago.innerHTML = `Nosso catalogo: [Ação] [Romance] [Comedia].`
+        }
+
+        botaoNao.onclick = function(){
+            catalago.innerHTML = "Ta bom"
+        }
+        catalago.appendChild(botaoSim)
+        catalago.appendChild(botaoNao)
     }
 }
